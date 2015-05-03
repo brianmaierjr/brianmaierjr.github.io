e0ed27f770586d220fa8e4e3f3d5c198bba2876f
Brian Maier <bmaier@TLH01618.local>
2015-05-03 17:11:43 -0400

    + fastclick.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,17 @@
 $(document).ready(function() {
 
+	// Nav Animation
 	$('.navicon-button').click(function() {
 		$(this).toggleClass('open');
 		$('nav').toggleClass('active'); 
 	});
 
+	// Smart Underlines
 	SmartUnderline.init({
 	  location: ''
 	})
+
+	// Init Fastclick.js
+	FastClick.attach(document.body);
 
 });
