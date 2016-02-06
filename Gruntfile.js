bc431a4cafdd4937f9466caa7cbc29b01d5b24d3
Brian Maier Jr <brimaidesigns@gmail.com>
2016-02-05 23:59:52 -0500

    update

--- Gruntfile.js
+++ Gruntfile.js
@@ -117,5 +117,6 @@ module.exports = function(grunt) {
 
     // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
     grunt.registerTask("default", ["concurrent:dev"]);
+    grunt.registerTask("production", ["jekyll:build"]);
 
 };
